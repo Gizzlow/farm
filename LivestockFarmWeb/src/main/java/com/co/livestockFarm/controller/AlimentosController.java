5060d83345bfeba6e074131a27477dc6de0025c9
CarlosEQ <55260129+CarlosEQ@users.noreply.github.com>
2021-08-25 22:27:53 -0500

    fixing conflicts

--- LivestockFarmWeb/src/main/java/com/co/livestockFarm/controller/AlimentosController.java
+++ LivestockFarmWeb/src/main/java/com/co/livestockFarm/controller/AlimentosController.java
@@ -2,31 +2,11 @@ package com.co.livestockFarm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.co.livestockFarm.dto.FoodDTO;
-import com.co.livestockFarm.dto.ResponseDTO;
-
-@RestController
-public class AlimentosController {
-	
-	@PostMapping("api/alimentos")
-	public ResponseDTO alimentos(@RequestBody FoodDTO alimento) {
-		ResponseDTO res = new ResponseDTO();
-		res.statusCode = 200;
-		res.message = "ssss";
-		res.object = alimento;
-		return res;
-=======
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import com.co.livestockFarm.dto.FoodDTO;
 import com.co.livestockFarm.service.AlimentosService;
 
 @Controller
@@ -58,7 +38,6 @@ public class AlimentosController {
 		}
 
 		return "Hello World";
->>>>>>> ad3f0b31d1a3a14c6787a17e6f7438efcb3fddde
 	}
 
 }
