baf9f67038b52e30f41d2dd59b675cf594b4a793
Gizzlow <jdtrujilloj@uqvirtual.edu.co>
2021-09-04 10:36:33 -0500

    Fix Materials

--- LivestockFarmWeb/src/main/java/com/co/livestockFarm/controller/MaterialsController.java
+++ LivestockFarmWeb/src/main/java/com/co/livestockFarm/controller/MaterialsController.java
@@ -1,8 +1,45 @@
 package com.co.livestockFarm.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.co.livestockFarm.dto.FoodDTO;
+import com.co.livestockFarm.dto.MaterialsDTO;
+import com.co.livestockFarm.service.MaterialsService;
+
+@RestController
+@RequestMapping(value = "/materials")
 public class MaterialsController {
 
+	@Autowired
+	private MaterialsService materialsService;
+
+	@PostMapping("/registerMaterials")
+	public ResponseEntity<?> registerMaterials(@RequestBody MaterialsDTO materials) {
+		try {
+			materialsService.getMaterialsByName(materials.getName());
+		} catch (Exception e) {
+
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).body(null);
+	}
+
+	@PostMapping("/addMaterials")
+	public String addMaterials(@RequestBody FoodDTO alimentos) {
+
+		try {
+
+		} catch (Exception e) {
+
+		}
+
+		return "Hello World";
+	}
 }
