bfe32cce46e52676a3b1559cef6d84b3e0ed8b78
Gizzlow <jdtrujilloj@uqvirtual.edu.co>
2022-03-06 17:27:22 -0500

    update

--- LivestockFarmData/src/main/java/com/co/livestockFarm/repository/ReportRepository.java
+++ LivestockFarmData/src/main/java/com/co/livestockFarm/repository/ReportRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import com.co.livestockFarm.dto.ReportFoodDTO;
+import com.co.livestockFarm.dto.ReportMaterialsDTO;
 import com.co.livestockFarm.dto.ReportMedicineDTO;
 import com.co.livestockFarm.dto.ReportTreatmentDTO;
 import com.co.livestockFarm.entity.HistoryFood;
@@ -23,8 +24,8 @@ public interface ReportRepository extends CrudRepository<HistoryMaterials, Long>
 	List<ReportMedicineDTO> getReportMedicine(@Param("fechaInicial") Date fechaInicial,
 			@Param("fechaFinal") Date fechaFinal);
 
-	@Query(value = "SELECT H FROM HistoryMaterials H WHERE CAST(H.date as date) BETWEEN :fechaInicial AND :fechaFinal ORDER BY H.date DESC")
-	List<HistoryMaterials> getReportMaterials(@Param("fechaInicial") Date fechaInicial,
+	@Query(value = "SELECT new com.co.livestockFarm.dto.ReportMaterialsDTO(M.name, H.date, H.observation, H.input, H.output, H.residue) FROM HistoryMaterials H JOIN Materials M ON H.materialsId = M.materialsId WHERE H.date BETWEEN :fechaInicial AND :fechaFinal ORDER BY H.date DESC")
+	List<ReportMaterialsDTO> getReportMaterials(@Param("fechaInicial") Date fechaInicial,
 			@Param("fechaFinal") Date fechaFinal);
 
 	@Query(value = "SELECT new com.co.livestockFarm.dto.ReportFoodDTO(F.name, H.date, H.input, H.output, H.balance, H.expirationDate, H.nombreAlmacen, H.icaRegistration, H.lote, H.observation) FROM HistoryFood H JOIN Food F ON H.foodId = F.foodId WHERE  CAST(H.date as date) BETWEEN :fechaInicial AND :fechaFinal ORDER BY H.date DESC")
